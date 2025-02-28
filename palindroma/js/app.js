823e8c09510e4a8d0b8c65eac5a3981da8a47b38
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-02-28 17:26:59 +0100

    feat: programma colmpletato

--- palindroma/js/app.js
+++ palindroma/js/app.js
@@ -1,1 +1,28 @@
-console.log('ciao Vincenzo')
+
+// utente inseriesce la parola 
+let userWord = prompt('Inserisci la parola')
+
+// inverto la parola
+
+function reverseWord(word) {
+    let newWord = '';
+ 
+    for (let i = word.length - 1; i >= 0; i--) { 
+        newWord += word[i]; 
+    }
+    
+    return newWord; 
+}
+
+ reverseWord(userWord)
+ console.log(reverseWord(userWord))
+ 
+ // verifico che la parola è palindroma e stampo il risultato
+ 
+if(userWord === reverseWord(userWord)){
+    console.log('la tua parola è palindroma');
+} else {
+    console.log('la tua parola non è palindroma');
+}
+
+ 
