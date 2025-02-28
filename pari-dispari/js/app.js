9756a4038e488f1bcd6ac86a479a5e74ce94ff1f
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-02-28 17:48:43 +0100

    feat: fatta funzione numeri casuali cpu

--- pari-dispari/js/app.js
+++ pari-dispari/js/app.js
@@ -1,1 +1,14 @@
-console.log('ciao Vincenzo')
+console.log('ciao vincenzo')
+
+
+
+let adjectiveNumber = '';
+let userNumber= 10;
+
+function cpuNumber(){
+   let number = Math.round(Math.random() * 5);
+    return number
+};
+
+cpuNumber();
+console.log(cpuNumber())
